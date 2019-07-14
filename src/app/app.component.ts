7501502e19ce30ab6bba0b2734b77be16c841f18
Georgios Andreadis <G.Andreadis@student.tudelft.nl>
2019-07-15 00:16:09 +0200

    Fix notch and add about page text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+declare var window: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -38,7 +40,16 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
+      this.statusBar.overlaysWebView(true);
       this.splashScreen.hide();
+
+      if (window.AndroidNotch) {
+        const style = document.documentElement.style;
+
+        window.AndroidNotch.getInsetTop(px => {
+          style.setProperty('--ion-safe-area-top', px + 'px');
+        }, (err) => console.error('Failed to get insets top:', err));
+      }
     });
   }
 }
